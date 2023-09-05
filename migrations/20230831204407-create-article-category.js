c5bc3144d8eba734286591825e8783bb304df673
ArangoMzl <sebastian.arango2411@gmail.com>
2023-09-05 14:33:03 -0500

    initial commit

---
+++ migrations/20230831204407-create-article-category.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('articleCategories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      articleId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {model: 'Articles', key: 'id'}
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {model: 'Categories', key: 'id'}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('articleCategories');
+  }
+};
